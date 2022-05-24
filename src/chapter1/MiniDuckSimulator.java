81a9b59fdb953809ab0f0542cca8cde05d779b6f
Gangbean <xbox2537@gamil.com>
2022-05-24 20:20:38 +0900

    feat : 행동 동적으로 제어하도록 수정
    1. Duck 클래스에 setter 추가
    2. Setter 활용해 런타임에서 위임클래스 동적 할당

--- src/chapter1/MiniDuckSimulator.java
+++ src/chapter1/MiniDuckSimulator.java
@@ -5,5 +5,13 @@ public class MiniDuckSimulator {
         Duck mallard = new MallardDuck();
         mallard.performFly();
         mallard.performQuack();
+
+        Duck modelDuck = new ModelDuck();
+        modelDuck.performFly();
+        modelDuck.setFlyBehavior(new FlyRocketPowered());
+        modelDuck.performFly();
+        modelDuck.performQuack();
+        modelDuck.setQuackBehavior(new Squeak());
+        modelDuck.performQuack();
     }
 }
